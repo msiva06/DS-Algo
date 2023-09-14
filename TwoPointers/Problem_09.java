d8ef0544f586dbc7ede9ce0215fdf8d0689448f3
msiva06 <m.sivasubramanian06@gmail.com>
2023-09-14 18:40:07 -0400

    Container with most water added

---
+++ TwoPointers/Problem_09.java
@@ -0,0 +1,18 @@
+package TwoPointers;
+
+// Container with most water
+public class Problem_09 {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while(left < right){
+            int currArea = Math.min(height[left],height[right]) * (right-left);
+            maxArea = Math.max(maxArea,currArea);
+            if(height[left] <= height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxArea;
+    }
+}
