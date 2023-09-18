15cb67c7794379a7914070011aeccc6f32495f04
msiva06 <m.sivasubramanian06@gmail.com>
2023-09-18 11:44:06 -0400

    Middle of Linkedlist added

---
+++ FastAndSlowPointers/Problem_03.java
@@ -0,0 +1,17 @@
+package FastAndSlowPointers;
+
+import TwoPointers.LinkedListNode;
+
+// Middle of LinkedList
+public class Problem_03 {
+    public static LinkedListNode middleNode(LinkedListNode head) {
+    
+    LinkedListNode slow = head;
+    LinkedListNode fast = head;
+    while(fast != null && fast.next != null){
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+}
