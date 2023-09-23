93095eec565651f8d35aa8ae5f8780e42399ad2b
msiva06 <m.sivasubramanian06@gmail.com>
2023-09-23 18:06:27 -0400

    LinkedListNode added'

---
+++ LinkedList/Problem_01.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class Problem_01 {
+    public static LinkedListNode reverse(LinkedListNode head) {
+
+        LinkedListNode prev = null;
+        LinkedListNode curr = head;
+        while(curr != null){
+            LinkedListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
