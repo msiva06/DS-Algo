b4e7af8cacbb7a39b240a78b5bc4d0d591d4a089
msiva06 <m.sivasubramanian06@gmail.com>
2023-10-28 13:28:25 -0400

    optimized code

--- TreeBFS/Problem_03.java
+++ TreeBFS/Problem_03.java
@@ -1,25 +1,40 @@
 // Populating next right pointers in each node
 public class Problem_03 {
     public static EduTreeNode<Integer> populateNextPointers(EduTreeNode<Integer> root) {
-		Queue<EduTreeNode<Integer>> queue = new LinkedList<>();
-		if(root == null)
-			return root;
-		queue.offer(root);
-		while(!queue.isEmpty()){
-			int size = queue.size();
-			for(int i = 0; i < size; i++){
-				EduTreeNode<Integer> node = queue.poll();
-				if(i != size-1){
-					node.next = queue.peek();
+		// Queue<EduTreeNode<Integer>> queue = new LinkedList<>();
+		// if(root == null)
+		// 	return root;
+		// queue.offer(root);
+		// while(!queue.isEmpty()){
+		// 	int size = queue.size();
+		// 	for(int i = 0; i < size; i++){
+		// 		EduTreeNode<Integer> node = queue.poll();
+		// 		if(i != size-1){
+		// 			node.next = queue.peek();
+		// 		}
+		// 		else{
+		// 			node.next = null;
+		// 		}
+		// 		if(node.left != null)
+		// 			queue.offer(node.left);
+		// 		if(node.right != null)
+		// 			queue.offer(node.right);
+		// 	}
+		// }
+		// return root;
+        EduTreeNode<Integer> leftMost = root;
+		while(leftMost.left != null){
+			EduTreeNode<Integer> curr = leftMost;
+			while(curr != null){
+				if(curr.left != null)
+					curr.left.next = curr.right;
+				if(curr.next != null){
+					curr.right.next = curr.next.left;
 				}
-				else{
-					node.next = null;
-				}
-				if(node.left != null)
-					queue.offer(node.left);
-				if(node.right != null)
-					queue.offer(node.right);
+				curr = curr.next;
 			}
+			leftMost = leftMost.left;
+			
 		}
 		return root;
 	}
